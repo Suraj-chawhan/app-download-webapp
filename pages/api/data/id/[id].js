1afd0ee2beb0fbba931dab7bfa7aaa006837f96e
Suraj-chawhan <surajchauhan442918@gmail.com>
2024-08-16 16:52:13 +0530

    new commit

--- pages/api/data/id/[id].js
+++ pages/api/data/id/[id].js
@@ -1,4 +1,4 @@
-import data from '../../../Component/Data.js'
+import data from '../../../../../Component/Data.js'
 export default function id(req,res){
  const {id}=req.query
  const singleApp=data.find(val=>val.id===Number(id))
