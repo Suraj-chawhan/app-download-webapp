520e3aff56ab28d55818b63889b9d6dff2a576bc
Suraj-chawhan <surajchauhan442918@gmail.com>
2024-08-16 16:09:44 +0530

    new commit

--- pages/app/[appId].js
+++ pages/app/[appId].js
@@ -1,51 +1,43 @@
-import {useRouter} from 'next/router'
-import App from '../../Component/App.js'
-import React from 'react'
-import Appdetails from '../../Component/Appdetails.js'
-import styles from '../../styles/home.module.css'
+import { useRouter } from 'next/router';
+import App from '../../Component/App.js';
+import React from 'react';
+import Appdetails from '../../Component/Appdetails.js';
+import styles from '../../styles/home.module.css';
 
-export default function Appid({apps,getid}){
-  
+export default function Appid({ apps, getid }) {
+  const router = useRouter();
 
-  const router=useRouter()
-  function handleClick(id){
-  router.push(`/app/${id}`)
+  function handleClick(id) {
+    router.push(`/app/${id}`);
   }
-  
-  
-const details=apps.find(val=>val.id===Number(getid))
- const recentdata=apps.filter(val=>val.tag==="recent")
- 
 
-    return(
+  const details = apps.find(val => val.id === Number(getid));
+  const recentdata = apps.filter(val => val.tag === "recent");
+
+  return (
     <div>
- 
-   <Appdetails  appdetails={details}/>
-   <h3>Recent</h3>
- <div className={styles.main}>
-{recentdata.map(val=><App app={val} call={(id)=>handleClick(id)} key={val.id}/>)}
- 
- </div>
+      <Appdetails appdetails={details} />
+      <h3>Recent</h3>
+      <div className={styles.main}>
+        {recentdata.map(val => (
+          <App app={val} call={(id) => handleClick(id)} key={val.id} />
+        ))}
+      </div>
     </div>
-    );
-  
+  );
 }
 
-
-
-
-
-
-export async function getServerSideProps(context){
-  const {params}=context
-
-  const response=await fetch(`https://app-download-webapp.vercel.app/api/data`)
-  const data=await response.json()
+export async function getServerSideProps(context) {
+  const { params } = context;
   
-  return{
-    props:{
-      getid:params.appId,
-      apps:data
-    }
-  }
+  // Corrected the API URL with `params.appId`
+  const response = await fetch(`https://app-download-webapp.vercel.app/api/data/${params.appId}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      getid: params.appId,
+      apps: data,
+    },
+  };
 }
